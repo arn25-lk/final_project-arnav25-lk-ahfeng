ec20bc901092362ce8980496afdac15094de01b7
Alex Feng <ahfeng@seas.upenn.edu>
2022-12-18 23:51:02 -0500

    commented out unfinished code to avoid errors

--- src/Graph.hs
+++ src/Graph.hs
@@ -7,7 +7,7 @@ import Control.Monad.RWS.Lazy (RWS, runRWS, evalRWS, ask, tell, get, put, MonadR
 import qualified Data.Text as T
 
 
-data Node = N {name:: String, getX :: Float, getY :: Float} deriving (Show, Eq)
+type Node = Int
 -- data Graph = G Vertices Edges deriving Show 
 -- newtype Vertices = V [Node]
 data Edge a = E {source::Node, dest:: Node, weight::a} deriving (Show, Eq)
@@ -17,66 +17,66 @@ type Path  = [Edge Float]
 
 
 
--- describe general heuristic 
-class Heuristic a where
-    euclid_distance  :: a -> a -> Float
-    manhattan_distance :: a -> a -> Float
+-- -- describe general heuristic 
+-- class Heuristic a where
+--     euclid_distance  :: a -> a -> Float
+--     manhattan_distance :: a -> a -> Float
 
 
-instance Heuristic Node where
-    euclid_distance :: Node -> Node -> Float
-    euclid_distance f g = sqrt ((getY g - getY f) * (getY g - getY f)  + (getX g - getX f) * (getX g - getX f))
+-- instance Heuristic Node where
+--     euclid_distance :: Node -> Node -> Float
+--     euclid_distance f g = sqrt ((getY g - getY f) * (getY g - getY f)  + (getX g - getX f) * (getX g - getX f))
 
 
 
--- Define traversible as things we can define an edge over
-class Traversible a where 
-    mkEdge :: (Num a, Ord a) => Node -> Node -> a -> Edge a
+-- -- Define traversible as things we can define an edge over
+-- class Traversible a where 
+--     mkEdge :: (Num a, Ord a) => Node -> Node -> a -> Edge a
 
 
-instance Traversible Float where
-    mkEdge :: Node -> Node -> Float -> Edge Float
-    mkEdge = E 
+-- instance Traversible Float where
+--     mkEdge :: Node -> Node -> Float -> Edge Float
+--     mkEdge = E 
     
     
 
-data SPConfig = SPConfig {
-    graph :: GraphMap Float,
-    startNodes :: [Node],
-    endNode :: [Node],
-    hDist :: [Float]
-} 
+-- data SPConfig = SPConfig {
+--     graph :: GraphMap Float,
+--     startNodes :: [Node],
+--     endNode :: [Node],
+--     hDist :: [Float]
+-- } 
 
-data OrderSnapshot = Snapshot {
-    current :: Node,
-    -- Explored nodes
-    expNodes :: [Node],
-    -- Explored edges 
-    expEdges :: Path,
-    -- Shortest path
-    spPath :: Path,
-    -- Best estimation path length (A*)
-    bestEstim :: Float, 
-    -- Graph 
-    graph :: GraphMap Float
-} deriving (Show)
-
-
-
--- reader shouldn't be start state, config file 
--- make rendering datatype 
-newtype SPContext st = SP { context :: RWS SPConfig (T.Text, OrderSnapshot) OrderSnapshot st } deriving (Monad, 
-                                                                        Functor, 
-                                                                        Applicative, 
-                                                                        MonadReader (SPConfig), 
-                                                                        MonadWriter (T.Text, OrderSnapshot), 
-                                                                        MonadState OrderSnapshot)
+-- data OrderSnapshot = Snapshot {
+--     current :: Node,
+--     -- Explored nodes
+--     expNodes :: [Node],
+--     -- Explored edges 
+--     expEdges :: Path,
+--     -- Shortest path
+--     spPath :: Path,
+--     -- Best estimation path length (A*)
+--     bestEstim :: Float, 
+--     -- Graph 
+--     graph :: GraphMap Float
+-- } deriving (Show)
 
 
 
+-- -- reader shouldn't be start state, config file 
+-- -- make rendering datatype 
+-- newtype SPContext st = SP { context :: RWS SPConfig (T.Text, OrderSnapshot) OrderSnapshot st } deriving (Monad, 
+--                                                                         Functor, 
+--                                                                         Applicative, 
+--                                                                         MonadReader (SPConfig), 
+--                                                                         MonadWriter (T.Text, OrderSnapshot), 
+--                                                                         MonadState OrderSnapshot)
 
-dfsStep ::  OrderSnapshot -> SPContext (OrderSnapshot)
-dfsStep = undefined
+
+
+
+-- dfsStep ::  OrderSnapshot -> SPContext (OrderSnapshot)
+-- dfsStep = undefined
     
 
 
@@ -86,27 +86,27 @@ dfsStep = undefined
 
 
 
--- Use monad transformers with a writer monad, as hardwired state monad is hard to change
--- Store snapshots of intermediate states
--- Look into graph module for generating well formed graphs for quickcheck
--- {Can use refinement types using LiquidHaskell}
--- Look into visualization using Gloss
--- Write type class interface for top level operations (like reading and writing to json)
--- More flexible node type class for heuristic computation 
+-- -- Use monad transformers with a writer monad, as hardwired state monad is hard to change
+-- -- Store snapshots of intermediate states
+-- -- Look into graph module for generating well formed graphs for quickcheck
+-- -- {Can use refinement types using LiquidHaskell}
+-- -- Look into visualization using Gloss
+-- -- Write type class interface for top level operations (like reading and writing to json)
+-- -- More flexible node type class for heuristic computation 
 
--- | Returns the sum of edge weights in a path
-pathLength :: Path -> Float
-pathLength = sum . map weight
+-- -- | Returns the sum of edge weights in a path
+-- pathLength :: Path -> Float
+-- pathLength = sum . map weight
 
-isCircular :: Path -> Bool
-isCircular = traverse [] where 
-    traverse visited [] =  False
-    traverse visited (e:es) =  (source e `elem` visited) || traverse (source e :visited) es 
+-- isCircular :: Path -> Bool
+-- isCircular = traverse [] where 
+--     traverse visited [] =  False
+--     traverse visited (e:es) =  (source e `elem` visited) || traverse (source e :visited) es 
 
 
 
--- Unit Tests
+-- -- Unit Tests
 
 
         
--- Arbitrary Instances
+-- -- Arbitrary Instances
